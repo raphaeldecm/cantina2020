8be49d22ec7c66d590e894b0cbf8cd33f2a24a92
Raphael Muniz <raphaeldecm@gmail.com>
2020-05-15 16:31:52 -0300

    Aleteração na posição dos alertas e modal de confirmação de operações.

--- src/main/java/com/raphael/cantina/controller/TurnoController.java
+++ src/main/java/com/raphael/cantina/controller/TurnoController.java
@@ -1,13 +1,8 @@
 package com.raphael.cantina.controller;
 
-import javax.servlet.ServletContext;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import com.raphael.cantina.model.Turno;
 import com.raphael.cantina.service.TurnoService;
 
-import org.codehaus.groovy.runtime.powerassert.SourceText;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -16,9 +11,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import org.thymeleaf.ITemplateEngine;
-import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.context.WebContext;
 
 @Controller
 @RequestMapping(value = "/turnos")
