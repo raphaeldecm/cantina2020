cc453eb20ca494e9327dff82690ebd0445021740
WeaknessesTestingTool <raphaeldecm@gmail.com>
2021-04-02 17:22:01 -0300

    layout modifications.

--- src/main/java/com/raphael/cantina/DemoApplication.java
+++ src/main/java/com/raphael/cantina/DemoApplication.java
@@ -1,7 +1,5 @@
 package com.raphael.cantina;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+//		System.setProperty("server.servlet.context-path", "/geces");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
